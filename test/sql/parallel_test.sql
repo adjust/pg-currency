b04e1d6c48667ff759d250b5b729d5c0700b5454
Chris Travers <chris.travers@gmail.com>
2017-11-20 12:09:02 +0100

    Parallel tests

---
+++ test/sql/parallel_test.sql
@@ -0,0 +1,19 @@
+BEGIN;
+SET max_parallel_workers_per_gather=4;
+SET force_parallel_mode=on;
+
+CREATE TABLE parallel_test(i int, c currency) WITH (parallel_workers = 4);
+INSERT INTO parallel_test (i, c)
+SELECT i, c.country
+FROM generate_series(1,1e6) i,
+LATERAL (SELECT CASE WHEN i % 3 = 0 THEN 'USD'::currency
+                     WHEN i % 3 = 1 THEN 'CAD'::currency
+                     WHEN i % 3 = 2 THEN 'EUR'::currency
+         END as country) c;
+
+EXPLAIN (costs off,verbose)
+SELECT COUNT(*) FROM parallel_test WHERE  c = 'USD';;
+
+EXPLAIN (costs off,verbose)
+SELECT c, COUNT(*) FROM parallel_test GROUP BY 1;
+ROLLBACK;
